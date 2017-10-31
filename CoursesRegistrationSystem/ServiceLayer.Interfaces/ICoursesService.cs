30d422ee087d4d3e8de37d363af3aa84a400d75c
Georgi Iliev <g.iliev@ultraplay.co>
2017-10-31 14:38:36 +0200

    Added ServiceLayer, ServiceLayer.Interfaces and ServiceLayer.Tests projects

---
+++ CoursesRegistrationSystem/ServiceLayer.Interfaces/ICoursesService.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceLayer.Interfaces
+{
+    using Models;
+    using System.Collections.Generic;
+
+    public interface ICoursesService
+    {
+        /// <summary>
+        /// Returns a list of all courses to which a user is registered
+        /// </summary>
+        /// <param name="username">Username of the user</param>
+        /// <returns>IEnumerable<Course></Course></returns>
+        IEnumerable<Course> GetAllRegisteredCoursesByUsername(string username);
+    }
+}
---
+++ CoursesRegistrationSystem/ServiceLayer/CoursesService.cs
@@ -0,0 +1,24 @@
+﻿namespace ServiceLayer
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Models;
+    using Interfaces;
+    using DataLayer.Interfaces;
+
+    public class CoursesService : ICoursesService
+    {
+        private ICoursesData data;
+
+        public CoursesService(ICoursesData data)
+        {
+            this.data = data;
+        }
+
+        public IEnumerable<Course> GetAllRegisteredCoursesByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
