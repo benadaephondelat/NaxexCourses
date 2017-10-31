6c217158015a18e16fc9b7dec22990e1357d515e
Georgi Iliev <g.iliev@ultraplay.co>
2017-10-31 13:39:43 +0200

    Added Models project

---
+++ CoursesRegistrationSystem/Models/ApplicationUser.cs
@@ -0,0 +1,45 @@
+﻿namespace Models
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class ApplicationUser : IdentityUser
+    {
+        private ICollection<Course> courses;
+
+        public ApplicationUser()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+
+        [Required]
+        public double CurrentStudentPoints { get; set; }
+
+        [Required]
+        public double StudentPointsLimit { get; set; }
+        
+        public virtual ICollection<Course> Courses
+        {
+            get
+            {
+                return this.courses;
+            }
+
+            set
+            {
+                this.courses = value;
+            }
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            return userIdentity;
+        }
+    }
+}
---
+++ CoursesRegistrationSystem/Models/Course.cs
@@ -0,0 +1,39 @@
+﻿namespace Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Course
+    {
+        private ICollection<ApplicationUser> registeredStudents;
+
+        public Course()
+        {
+            this.RegisteredStudents = new HashSet<ApplicationUser>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string CourseName { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public double CoursePoints { get; set; }
+
+        public virtual ICollection<ApplicationUser> RegisteredStudents
+        {
+            get
+            {
+                return this.registeredStudents;
+            }
+
+            set
+            {
+                this.registeredStudents = value;
+            }
+        }
+    }
+}
