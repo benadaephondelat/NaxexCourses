649867a98728dea283192ca5ce70e9ad0ffcdaa8
Georgi Iliev <g.iliev@ultraplay.co>
2017-10-31 13:24:45 +0200

    Created Presentation Layer Courses.Web and Courses.Web.Tests Added /packages folder to the .gitignore file

---
+++ CoursesRegistrationSystem/Courses.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Courses.Web.Startup))]
+namespace Courses.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
